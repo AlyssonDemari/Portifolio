2765c198de95d0af8b2b87715fff5e1dcab30534
Alysson Demari <alyssondemari@gmail.com>
2024-09-26 19:07:52 -0300

    mexendo no hard skills

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const progressoHTML = document.querySelectorAll(".barra div")[0];
-progressoHTML.setAttribute("style", "width: 80%;"); // 80% para HTML
+progressoHTML.setAttribute("style", "width: 90%;");
 
 const progressoCSS = document.querySelectorAll(".barra div")[1];
-progressoCSS.setAttribute("style", "width: 90%;"); // 90% para CSS
+progressoCSS.setAttribute("style", "width: 90%;");
 
-const progressoJS = document.querySelectorAll(".barra div")[3];
-progressoJS.setAttribute("style", "width: 10%;"); // 70% para JavaScript
+const progressoJS = document.querySelectorAll(".barra div")[2];
+progressoJS.setAttribute("style", "width: 50%;"); 
 
-const progressoREACT = document.querySelectorAll(".barra div")[2];
-progressoREACT.setAttribute("style", "width: 70%;"); // 70% para JavaScript
+const progressoREACT = document.querySelectorAll(".barra div")[3];
+progressoREACT.setAttribute("style", "width: 0%;"); 
 
 const progressoMSQL = document.querySelectorAll(".barra div")[4];
-progressoMSQL.setAttribute("style", "width: 8%;"); // 70% para JavaScript
+progressoMSQL.setAttribute("style", "width: 0%;"); 
 
 const progressoPYTHON = document.querySelectorAll(".barra div")[5];
-progressoPYTHON.setAttribute("style", "width: 30%;"); // 70% para JavaScript
+progressoPYTHON.setAttribute("style", "width: 30%;"); 

d290d9e349d13017397e2642f7eb432c72e08822
Alysson Demari <alyssondemari@gmail.com>
2024-09-26 21:39:03 -0300

    centralizando o texto do progresso

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ const progressoCSS = document.querySelectorAll(".barra div")[1];
 progressoCSS.setAttribute("style", "width: 90%;");
 
 const progressoJS = document.querySelectorAll(".barra div")[2];
-progressoJS.setAttribute("style", "width: 50%;"); 
+progressoJS.setAttribute("style", "width: 50%;");
 
-const progressoREACT = document.querySelectorAll(".barra div")[3];
+ const progressoPYTHON = document.querySelectorAll(".barra div")[3];
+progressoPYTHON.setAttribute("style", "width: 30%;"); 
+
+const progressoREACT = document.querySelectorAll(".barra div")[4];
 progressoREACT.setAttribute("style", "width: 0%;"); 
 
-const progressoMSQL = document.querySelectorAll(".barra div")[4];
+const progressoMSQL = document.querySelectorAll(".barra div")[5];
 progressoMSQL.setAttribute("style", "width: 0%;"); 
 
-const progressoPYTHON = document.querySelectorAll(".barra div")[5];
-progressoPYTHON.setAttribute("style", "width: 30%;"); 
+

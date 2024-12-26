84b869f7c227090750729b3456d6a09b66884d7a
Alysson Demari <alyssondemari@gmail.com>
2024-12-26 16:28:37 -0300

    arrumando slide do grid

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
 // nav
 
 window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem',window.scrollY > 100)
+  let header = document.querySelector('#header')
+  header.classList.toggle('rolagem',window.scrollY > 100)
 })
 
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+handleClick() {
+  this.navList.classList.toggle(this.activeClass);
+  this.mobileMenu.classList.toggle(this.activeClass);
+  this.animateLinks();
+}
+
+addClickEvent() {
+  this.mobileMenu.addEventListener("click", this.handleClick);
+}
+
+init() {
+  if (this.mobileMenu) {
+    this.addClickEvent();
+  }
+  return this;
+}
+}
   
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
 
 
-let currentSlide = 0;
-const items = document.querySelectorAll('.container-proj');
-const totalSlides = items.length;
-const visibleItems = 7;
-const itemWidth = 240;
+//? Função responsável pela movimentação do grid
+const containerItem = document.getElementById('container-item');
+const itemsWrapper = containerItem.querySelector('.items-wrapper');
+const totalItems = document.querySelectorAll('.container-proj').length;
+let offset = 0; //! Acompanhando a posição dos itens
+const itemWidth = 490; //! Largura do item para deslocamento
+const visibleItems = 11; //! mudar para caso acrescente mais projetos sempre 4 a mais que o número do     background-color: pink;
+const maxOffset = (totalItems - visibleItems) * itemWidth; 
 
-document.getElementById('prev').addEventListener('click', () => {
-    if (currentSlide > 0) {
-        currentSlide--;
-        items.forEach(item => {
-            item.style.transform = `translateX(${-currentSlide * itemWidth}px)`;
-        });
-    }
-});
+function next() {
+  if (offset > -maxOffset) {
+    offset -= itemWidth; 
+    itemsWrapper.style.transform = `translateX(${offset}px)`;
+  }
+}
 
-document.getElementById('next').addEventListener('click', () => {
-    if (currentSlide < totalSlides - visibleItems) {
-        currentSlide++;
-        items.forEach(item => {
-            item.style.transform = `translateX(${-currentSlide * itemWidth}px)`;
-        });
-    }
-});
+function prev() {
+  if (offset < 0) { 
+    offset += itemWidth; 
+    itemsWrapper.style.transform = `translateX(${offset}px)`;
+  }
+}
+//? -- FIM -- Função responsável pela movimentação do grid
 
 
 // Progrsso 
